b6eac2ebc9f997794e4b85e32b9858e3d1088fcd
Kyla Ulibarri <k.barri.071@gmail.com>
2018-07-23 23:09:20 +0000

    automatically backed up by learn

--- macarena.rb
+++ macarena.rb
@@ -32,4 +32,6 @@ puts "Step 7: Bring your right hand up and around to the back of your head."
 puts "Step 8: Bring your left hand up and around to the back your head"
 end
 
+puts macarena
+puts "turn to the left"
 
